72c7d07439bd5deff7a7e13caa92eb8b2136108e
Tarun Naidu g <117089439+tharunnaidug@users.noreply.github.com>
2025-02-26 16:19:19 +0530

    Minor Auction Route Added

---
+++ controllers/auction.controllers.js
@@ -0,0 +1,16 @@
+import auctionModel from '../models/auction.model';
+
+export const allMyListedAuctions = async (req, res) => {
+    let userId = req.user._id;
+
+    if (!userId)
+        return res.status(401).json({ error: "No UserId Found" })
+    try {
+        let auctions = auctionModel.find({ seller: userId })
+
+        return res.status(200).json({ message: "success", auctions })
+    } catch (error) {
+        console.log("problem in Getting All listed Auctions ", error)
+        res.status(500).json({ error: "Internal Server Error" })
+    }
+}
--- routes/auctionRoutes.js
+++ routes/auctionRoutes.js
@@ -1,7 +1,9 @@
 import express, { Router } from "express"
+import { verifyUser } from "../middlewares/isLoggedIn";
+import { allMyListedAuctions } from "../controllers/auction.controllers";
 
 const router=express.Router();
 
-
+router.get('/allMyListedAuctions',verifyUser,allMyListedAuctions)
 
 export default router;
