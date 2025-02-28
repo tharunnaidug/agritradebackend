caca6434738d2a76a758119820239680b175c98e
Tarun Naidu g <117089439+tharunnaidug@users.noreply.github.com>
2025-02-28 20:22:47 +0530

    Changes In Auction Module

--- controllers/auction.controllers.js
+++ controllers/auction.controllers.js
@@ -6,11 +6,30 @@ export const allMyListedAuctions = async (req, res) => {
     if (!userId)
         return res.status(401).json({ error: "No UserId Found" })
     try {
-        let auctions = auctionModel.find({ seller: userId })
+        const pastAuctions = await Auction.find({
+            auctionDateTime: { $lt: new Date() },
+            $or: [{ seller: userId }, { bids: { $in: [userId] } }]
+        });
 
-        return res.status(200).json({ message: "success", auctions })
+        const upcomingAuctions = await Auction.find({
+            auctionDateTime: { $gt: new Date() },
+            $or: [{ seller: userId }, { bids: { $in: [userId] } }]
+        });
+
+        return res.status(200).json({ message: "success", pastAuctions,upcomingAuctions })
     } catch (error) {
         console.log("problem in Getting All listed Auctions ", error)
         res.status(500).json({ error: "Internal Server Error" })
+    }
+}
+export const viewAuction=async(req,res)=>{
+    try {
+        let aucId=req.params.id;
+        const auction = await auctionModel.findById(aucId).populate("seller", "name").populate("bids");
+        if (!auction) return res.status(404).json({ message: "Auction not found" });
+        return res.status(200).json({ message: "success", auction })
+    } catch (error) {
+        console.log("problem in Getting A Auction ", error)
+        res.status(500).json({ error:"Internal Server Error" });
     }
 }
--- routes/auctionRoutes.js
+++ routes/auctionRoutes.js
@@ -1,9 +1,11 @@
 import express, { Router } from "express"
 import { verifyUser } from "../middlewares/isLoggedIn.js";
-import { allMyListedAuctions } from "../controllers/auction.controllers.js";
+import { allMyListedAuctions, viewAuction } from "../controllers/auction.controllers.js";
 
 const router=express.Router();
 
 router.get('/allMyListedAuctions',verifyUser,allMyListedAuctions)
+
+router.get('/auction/:id',verifyUser,viewAuction)
 
 export default router;
