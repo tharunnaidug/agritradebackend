97094784f82a43a1af51b7121570f6608152de14
Tarun Naidu g <117089439+tharunnaidug@users.noreply.github.com>
2025-04-01 11:13:29 +0530

    ...

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ mongoose.connect(process.env.DBURL)
   app.use(cookieParser());
   
   app.use(cors({
-    origin: "https://agritrade-three.vercel.app/",
+    origin: "https://agritrade-three.vercel.app",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"]
